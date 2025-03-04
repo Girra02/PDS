de9d9f8ff464bc644fca0ecdd4603462256ae32d
Girra02 <111903296+Girra02@users.noreply.github.com>
2025-03-03 22:27:12 -0600

    Create data_visualization.py

---
+++ assignment1/question1/frailty_data/src/data_visualization.py
@@ -0,0 +1,14 @@
+# 1. Histogram of Grip Strength
+plt.figure(figsize=(8,5))
+sns.histplot(df["Grip strength"], bins=5, kde=True)
+plt.title("Distribution of Grip Strength")
+plt.xlabel("Grip Strength (kg)")
+plt.ylabel("Count")
+plt.savefig("/content/histogram_grip_strength.png")
+plt.show()
+
+plt.figure(figsize=(6,4))
+sns.heatmap(df.corr(), annot=True, cmap="coolwarm", linewidths=0.5)
+plt.title("Feature Correlation Heatmap")
+plt.savefig("/content/heatmap_correlation.png")
+plt.show()
